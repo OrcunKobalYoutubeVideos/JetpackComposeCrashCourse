5269c93e1fed7a791e12b672af9d86544ee0770e
Orcun Kobal <orcunk@powergroup.com.tr>
2024-09-29 18:40:28 +0300

    bölüm 2 final

--- app/src/main/java/com/youtube/crashcourse/MainActivity.kt
+++ app/src/main/java/com/youtube/crashcourse/MainActivity.kt
@@ -1,35 +1,33 @@
 package com.youtube.crashcourse
 
-import android.graphics.Paint.Align
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.youtube.crashcourse.ui.theme.YoutubeCrashCourseTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,10 +49,24 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun App() {
-        
+    fun App(vm:MyViewModel = viewModel()) {
+        val isim by vm.isim.collectAsState()
+        OutlinedTextField(value = isim, onValueChange = {changedValue ->
+            vm.ismiGuncelle(changedValue)
+        })
+        Text(text = isim)
     }
 
+}
 
+class MyViewModel : ViewModel(){
+    private val _isim = MutableStateFlow("")
+    val isim: StateFlow<String> = _isim
+
+    fun ismiGuncelle(isim:String){
+        _isim.update {
+            isim
+        }
+    }
 }
 
